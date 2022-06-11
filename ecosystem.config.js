4d5bd51665a13cb75affa10022e943627913a948
Vitalii Braun <vitossvx@gmail.com>
2022-06-11 18:05:21 +0300

    ecosystem-config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'pizza',
+            name: 'champion-pizza',
             script: 'server.js',
             instances: 1,
             autorestart: true,
