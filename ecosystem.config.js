9d8ed829497b7d683511d9c0d80c1176eb44fc1b
Vitalii Braun <vitossvx@gmail.com>
2022-06-26 12:42:24 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,17 @@
 module.exports = {
     apps: [
         {
+            name: 'champion-pizza-dev',
+            script: 'server.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '300M',
+            env_development: {
+                NODE_ENV: "development"
+            }
+        },
+        {
             name: 'champion-pizza',
             script: 'server.js',
             instances: 1,
